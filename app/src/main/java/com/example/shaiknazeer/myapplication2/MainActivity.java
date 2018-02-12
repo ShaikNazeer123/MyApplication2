a9483980c70b8f4f62026e8f7022d62d39fe600c
ShaikNazeer123 <shaik.nazeer2015@vit.ac.in>
2018-02-12 18:06:17 +0530

    line 75 76 mainAc

--- app/src/main/java/com/example/shaiknazeer/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/shaiknazeer/myapplication2/MainActivity.java
@@ -72,6 +72,8 @@ public class MainActivity extends AppCompatActivity
         }
         return super.onOptionsItemSelected(item);
     }
+    //kjhrgb
+    //iuubjb
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
